f8ead5cd582e8d8a32ee66f9ef4f4de7fa9a34b6
fstipo <fstipanov@gmail.com>
2022-06-03 11:26:20 +0200

    setTimeout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(1);
+  console.log(2);
+  console.log(3);
+  setTimeout(() => console.log('callback'), 2000);
+  console.log(4);
+  console.log(5);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App my-3 display-2"></div>
+    </>
   );
 }
 

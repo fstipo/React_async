d8351b9a7c3c49b5089f01f760b2eab8bbee6845
fstipo <fstipanov@gmail.com>
2022-06-03 11:55:04 +0200

    XMLHttpRequest()

---
+++ 02. XMLHttpRequest( )/Api.js
@@ -0,0 +1,11 @@
+export const fetchData = () => {
+  const request = new XMLHttpRequest();
+  request.addEventListener('readystatechange', () => {
+    //     console.log(request, request.readyState);
+    if (request.readyState === 4) {
+      console.log(request.responseText);
+    }
+  });
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+  request.send();
+};

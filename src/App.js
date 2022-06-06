3b2c9b0205e34638b1cac8d76515bbab854e044c
fstipo <fstipanov@gmail.com>
2022-06-06 12:16:51 +0200

    Promises and promise chain

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { fetchData } from './component/Api';
 import './App.css';
 
-fetchData('https://jsonplaceholder.typicode.com/todos/1', (err, data) => {
-  console.log('Callback 1. is fired');
-  console.log(data);
-  fetchData('https://jsonplaceholder.typicode.com/todos/2', (err, data) => {
-    console.log('Callback 2. is fired');
-    console.log(data);
-    fetchData('https://jsonplaceholder.typicode.com/todos/3', (err, data) => {
-      console.log('Callback 3. is fired');
-      console.log(data);
-    });
-  });
-});
+// fetchData('https://jsonplaceholder.typicode.com/todos/1', (err, data) => {
+//   console.log('Callback 1. is fired');
+//   console.log(data);
+//   fetchData('https://jsonplaceholder.typicode.com/todos/2', (err, data) => {
+//     console.log('Callback 2. is fired');
+//     console.log(data);
+//     fetchData('https://jsonplaceholder.typicode.com/todos/3', (err, data) => {
+//       console.log('Callback 3. is fired');
+//       console.log(data);
+//     });
+//   });
+// });
 
 function App() {
   return (

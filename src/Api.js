051ac80e5dc33dfa83754381c0c4f4fa802804e5
fstipo <fstipanov@gmail.com>
2022-06-06 12:46:15 +0200

    fetch data

---
+++ src/Api.js
@@ -0,0 +1,18 @@
+export const fetchData = (resource, callback) => {
+  const request = new XMLHttpRequest();
+  request.addEventListener('readystatechange', () => {
+    //     console.log(request, request.readyState);
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request);
+      const data = JSON.parse(request.responseText);
+      // console.log('✔', request.responseText);
+      callback(undefined, data);
+    } else if (request.readyState === 4) {
+      // console.log("⛔ Can't fetch the data ");
+      callback('⛔ could not fetch the data', undefined);
+    }
+  });
+  //request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+  request.open('GET', 'todos.json');
+  request.send();
+};
